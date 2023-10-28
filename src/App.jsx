251e32dcd2e3e82ccb1620b6279746e17006d7a6
ShakaPursuit <shakalivngstone@pursuit.org>
2023-10-27 23:44:04 -0400

    added singleTransaction Component with Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './Components/Header'
 import './index.css'
 import NewTransaction from './Components/NewTransactions'
 import ShowAllTransactions from './Components/ShowAllTransactions'
+import SingleTransaaction from './Components/TransactionCard'
 
 function App() {
  
@@ -16,7 +17,8 @@ function App() {
        <Header/> 
          <Routes>
             <Route path="/transactions/new" element={<NewTransaction/>}/>
-            <Route path="transactions" element={<ShowAllTransactions/>}/>
+            <Route path="/transactions" element={<ShowAllTransactions/>}/>
+            <Route path="/transactions/:index" element={<SingleTransaaction/>}/>
           </Routes>
        </Router> 
     </>
--- src/Components/TransactionCard.jsx
+++ src/Components/TransactionCard.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 
 
-const SingleTransaaction=()=>{
+const SingleTransaaction = () => {
+
+
+    const [transaction, setTransaction] = useState([])
+    const { index } = useParams()
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8000/transactions/`);
+                if (!response.ok) {
+                    throw new Error(`Request failed with status: ${response.status}`);
+                }
+                const data = await response.json();
+                setTransaction(data);
+            } catch (error) {
+                console.error('Fetch error:', error)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+
+    return (<>
+        {transaction.map((item, specificIndex) => {
+            if (index == specificIndex) {
+                return (<div className="single" key={index} id={index}>
+
+
+
+
+                    <div id="date">{`${item.date}`}&nbsp;&nbsp;</div>
+                    <div id="company"> {`${item.item_name}`}&nbsp;&nbsp;-&nbsp;{item.from}</div>
+                    <div id="amount">  {`${item.amount}`}</div>
+
+
+
+
+                </div>)
+
+
+            }
 
 
 
 
 
 
-    return(<>
-    
-    
-    
+        })}
+
+        <div>
+
+        </div>
+
+
+
     </>)
 }
 
-export default(SingleTransaaction)
+export default SingleTransaaction
